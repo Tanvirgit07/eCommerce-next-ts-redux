d792164a4933f5cf4c3f74135c80344c31ee9a27
Tanvir Ahmmed <tanvirgithub2024@gmail.com>
2025-04-11 00:03:58 +0600

    complete signIn and cart rediratc

--- src/Components/Header/BottomHeader.tsx
+++ src/Components/Header/BottomHeader.tsx
@@ -1,8 +1,14 @@
+import { auth } from "@/app/api/auth/[...nextauth]/route";
 import { navigation } from "@/Constant";
 import Link from "next/link";
 import React from "react";
+import SignOut from "../SignOut";
 
-function BottomHeader() {
+
+
+const BottomHeader = async() =>{
+  const session = await auth()
+  console.log(session);
   return (
     <div className="border-b border-b-gray-400">
       <div className="max-w-[1140px] mx-auto px-4 flex items-center justify-between py-1">
@@ -16,9 +22,10 @@ function BottomHeader() {
               {item.title}
             </Link>
           ))}
-          <Link className="hover:text-blue-500 duration-200" href="/signin">
+          {session?.user ?<SignOut key={session?.user?.id}/> :<Link className="hover:text-blue-500 duration-200" href="/signin">
             Please signin to view cart
-          </Link>
+          </Link>}
+          
         </div>
 
         <p className="hidden md:inline-flex text-xs text-gray-400 font-medium">
--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,7 +1,13 @@
 import CartProducts from '@/Components/cartPage/CartProducts'
 import React from 'react'
+import { auth } from '../api/auth/[...nextauth]/route'
+import { redirect } from 'next/navigation';
 
-const CartPage = () => {
+const CartPage = async() => {
+  const session = await auth();
+  if(!session?.user){
+    redirect("/");
+  }
   return (
     <div className=''>
         <CartProducts />
